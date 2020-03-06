8062dcbd97c540daf0bd923306446705523f7978
e-m-s-y <hofman.gb@gmail.com>
2020-03-06 19:06:27 +0100

    Refactored settings

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,1 +1,45 @@
 $.tabGroup.open();
+
+if( ! Ti.App.Properties.getObject('passphrase', false)) {
+	const dialog = Ti.UI.createAlertDialog({
+		title: 'Please set your passphrase of your wallet. You can change this value in the settings tab.',
+		style: Ti.UI.iOS.AlertDialogStyle.PLAIN_TEXT_INPUT,
+		buttonNames: ['OK']
+	});
+
+	dialog.addEventListener('click', function(event) {
+		if(event.text.length) {
+			Ti.App.Properties.setObject('passphrase', event.text);
+		}
+
+		promptNonce();
+	});
+
+	dialog.show();
+} else {
+	promptNonce();
+}
+
+function promptNonce() {
+	if( ! Ti.App.Properties.getObject('nonce', false)) {
+		const dialog = Ti.UI.createAlertDialog({
+			title: 'Set your nonce. You can also change this value in the settings tab.',
+			style: Ti.UI.iOS.AlertDialogStyle.PLAIN_TEXT_INPUT,
+			buttonNames: ['OK']
+		});
+
+		dialog.addEventListener('click', function(e) {
+			if(e.text.length) {
+				const nonce = parseInt(e.text);
+
+				if( ! Number.isInteger(nonce)) {
+					return alert('Invalid nonce, please try again.');
+				}
+
+				Ti.App.Properties.setObject('nonce', nonce);
+			}
+		});
+
+		dialog.show();
+	}
+}
--- app/controllers/settings.js
+++ app/controllers/settings.js
@@ -1,6 +1,93 @@
-const sessionTemplate = {
-	nonce: 0
-};
+function promptPassphrase() {
+	if(OS_IOS) {
+		const dialog = Ti.UI.createAlertDialog({
+			title: 'Set new passphrase',
+			style: Ti.UI.iOS.AlertDialogStyle.PLAIN_TEXT_INPUT,
+			buttonNames: ['OK']
+		});
+
+		dialog.addEventListener('click', function(e) {
+			if( ! e.text.length) {
+				return;
+			}
+
+			Ti.App.Properties.setObject('passphrase', e.text);
+			reloadList();
+		});
+
+		dialog.show();
+	} else {
+		// TODO show alertdialog with androidView https://wiki.appcelerator.org/display/guides2/AlertDialog
+	}
+}
+
+function promptNonce() {
+	if(OS_IOS) {
+		const dialog = Ti.UI.createAlertDialog({
+			title: 'Set new nonce',
+			style: Ti.UI.iOS.AlertDialogStyle.PLAIN_TEXT_INPUT,
+			buttonNames: ['OK']
+		});
+
+		dialog.addEventListener('click', function(e) {
+			if( ! e.text.length) {
+				return;
+			}
+
+			const nonce = parseInt(e.text);
+
+			if( ! Number.isInteger(nonce)) {
+				return alert('Invalid nonce, please try again.');
+			}
+
+			Ti.App.Properties.setObject('nonce', nonce);
+			reloadList();
+		});
+
+		dialog.show();
+	} else {
+		// TODO show alertdialog with androidView https://wiki.appcelerator.org/display/guides2/AlertDialog
+	}
+}
+
+function loadWalletSection() {
+	$.listView.sections[0].items = [{
+		title: {text: 'Passphrase'},
+		subTitle: {text: Ti.App.Properties.getObject('passphrase', '')},
+		template: 'doubleWithClick',
+		payload: {callback: promptPassphrase}
+	}, {
+		title: {text: 'Nonce'},
+		subTitle: {text: Ti.App.Properties.getObject('nonce', '')},
+		template: 'doubleWithClick',
+		payload: {callback: promptNonce}
+	}];
+}
+
+function hardReset() {
+	Ti.App.Properties.removeProperty('nonce');
+	Ti.App.Properties.removeProperty('passphrase');
+	reloadList();
+	alert('All session data has been removed.');
+}
+
+function loadGeneralSection() {
+	$.listView.sections[1].items = [{
+		title: {text: 'App version'},
+		subTitle: {text: Ti.App.version},
+		template: 'double'
+	}, {
+		title: {text: 'Hard reset'},
+		subTitle: {text: 'This will erase all session data'},
+		template: 'doubleWithClick',
+		payload: {callback: hardReset}
+	}];
+}
+
+function reloadList() {
+	loadWalletSection();
+	loadGeneralSection();
+}
 
 function onItemClickHandler(event) {
 	const listItem = $.listView.sections[event.sectionIndex].getItemAt(event.itemIndex);
@@ -9,58 +96,3 @@ function onItemClickHandler(event) {
 		listItem.payload.callback();
 	}
 }
-
-$.listView.sections[0].items = [{
-	title: {text: 'Passphrase'},
-	subTitle: {text: 'TODO'},
-	template: 'doubleWithClick'
-}, {
-	title: {text: 'Nonce'},
-	subTitle: {text: Ti.App.Properties.getObject('session', sessionTemplate).nonce},
-	template: 'doubleWithClick',
-	payload: {
-		callback: function() {
-			if(OS_IOS) {
-				const dialog = Ti.UI.createAlertDialog({
-					title: 'Set new nonce',
-					style: Ti.UI.iOS.AlertDialogStyle.PLAIN_TEXT_INPUT,
-					buttonNames: ['OK']
-				});
-
-				dialog.addEventListener('click', function(e) {
-					const input = e.text;
-
-					if( ! input.length) {
-						return;
-					}
-
-					const session = Ti.App.Properties.getObject('session', sessionTemplate);
-					const nonce = parseInt(e.text);
-
-					if( ! Number.isInteger(nonce)) {
-						return alert('Invalid nonce, please try again.');
-					}
-
-					session.nonce = nonce;
-					const items = $.listView.sections[0].items;
-					items[1].subTitle.text = nonce;
-					$.listView.sections[0].items = items;
-
-					Ti.App.Properties.setObject('session', session);
-				});
-
-				dialog.show();
-
-
-			} else {
-				// TODO show alertdialog with androidView https://wiki.appcelerator.org/display/guides2/AlertDialog
-			}
-		}
-	}
-}];
-
-$.listView.sections[1].items = [{
-	title: {text: 'App version'},
-	subTitle: {text: Ti.App.version},
-	template: 'double'
-}];
