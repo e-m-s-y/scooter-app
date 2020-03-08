1ebcc9ab0b8596d857b2814894d22a91f61badd9
e-m-s-y <hofman.gb@gmail.com>
2020-03-08 11:59:05 +0100

    Added duration

--- app/controllers/ride/details.js
+++ app/controllers/ride/details.js
@@ -1,6 +1,22 @@
-console.log($.args);
+const moment = require('alloy/moment');
 
-$.recipient.text = $.recipient.text + $.args.recipientId;
-$.sessionId.text = $.sessionId.text + $.args.asset.sessionId;
-$.rate.text = $.rate.text + $.args.asset.rate;
-$.gps.text = $.gps.text + JSON.stringify($.args.asset.gps);
+function setDuration() {
+	const start = moment($.args.rentalStartTx.asset.gps.human);
+	const duration = moment.utc(moment().diff(start));
+
+	$.duration.text = 'Duration: ' + duration.format('HH:mm:ss');
+}
+
+const interval = setInterval(function() {
+	setDuration();
+}, 1000);
+
+function onCloseHandler() {
+	clearInterval(interval);
+}
+
+$.recipient.text = $.recipient.text + $.args.rentalStartTx.recipientId;
+$.sessionId.text = $.sessionId.text + $.args.rentalStartTx.asset.sessionId;
+$.rate.text = $.rate.text + $.args.rentalStartTx.asset.rate;
+$.gps.text = $.gps.text + JSON.stringify($.args.rentalStartTx.asset.gps);
+setDuration();
